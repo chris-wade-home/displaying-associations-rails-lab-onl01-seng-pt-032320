f57b7a389c80a2c3570cf5c9cfdc309bc023950d
swade781977 <swade781977@yahoo.com>
2020-08-19 18:29:57 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180913134502) do
+ActiveRecord::Schema.define(version: 20200819201533) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -20,9 +20,10 @@ ActiveRecord::Schema.define(version: 20180913134502) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "artist_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "artist_id_id"
+    t.index ["artist_id_id"], name: "index_songs_on_artist_id_id"
   end
 
 end
